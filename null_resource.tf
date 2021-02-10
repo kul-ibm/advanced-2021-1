07b7664d80b5475b14974054e5c36de0f609b233
Kulbhushan <kulbhushan.mayer@>
2021-02-10 17:07:45 +0530

    added null_resource to add provisioner to mount EBS on EC2

---
+++ null_resource.tf
@@ -0,0 +1,15 @@
+resource "null_resource" "mountEBS" {
+  provisioner "remote-exec" {
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      host = aws_instance.server.public_ip
+      private_key = file("c:/training/kul-labs.pem")
+    }
+    inline = [
+      "sudo mkfs -t ext4 /dev/xvdf",
+      "sudo mount /dev/xvdf /opt",
+      "sudo chown ubuntu:ubuntu /opt"
+    ]
+  }
+}

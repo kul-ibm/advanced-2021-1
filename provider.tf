240f75df5e4c49b97bf66b46d3f82a87886ea013
Kulbhushan <kulbhushan.mayer@>
2021-02-11 10:23:41 +0530

    added S3 as remote backend to manage tfstate file and dynamoDB to be used for locking the state file

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-east-2"
 }
+
+terraform {
+  backend "s3"{
+    bucket = "thinknyx"
+    key = "ibm-da-feb/kul.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "terraform-kul"
+  }
+}

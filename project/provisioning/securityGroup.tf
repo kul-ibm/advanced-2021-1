7a1071d918a9071f34bd8251c7e9353d07d0656d
Kulbhushan <kulbhushan.mayer@>
2021-02-12 09:58:35 +0530

    added security group and provider

---
+++ project/provisioning/securityGroup.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "sg" {
+  name = var.tagName
+  description = "K8S Security Group, Managed by Terraform"
+  tags = {
+    "Name" = var.tagName
+  }
+  ingress {
+      from_port = "6443"
+      to_port = "6443"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+      from_port = "2379"
+      to_port = "2380"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+      from_port = "10250"
+      to_port = "10252"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+      from_port = "30000"
+      to_port = "32767"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+      from_port = "53"
+      to_port = "53"
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+      from_port = "53"
+      to_port = "53"
+      protocol = "udp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+      from_port = "0"
+      to_port = "0"
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ project/provisioning/variables.tf
@@ -0,0 +1,4 @@
+variable "tagName" {
+  description = "Name to be used while creating Services and applying tags, e.g. thinknyx-kul"
+  type = string
+}
